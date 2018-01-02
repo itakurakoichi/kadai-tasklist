9a2773de0b9c5eb10769a49a9af49e61fce8fe7e
jiro.kirameki <itakurakoichi.s@gmail.com>
2018-01-02 16:39:13 +0000

    ログイン機能、sessions#new sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,37 @@ class SessionsController < ApplicationController
   end
 
   def create
+    hoge = params[:session]
+    email = params[:session][:email].downcase
+    password = params[:session][:password]
+    if login(email, password)
+      flash[:success] = 'ログインに成功しました。 >>> #{hoge}'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ログインに失敗しました。'
+      render 'new'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def login(email, password)
+    # 入力フォームの email と同じメールアドレスを持つユーザを検索し @user へ代入、無ければ nil
+    @user = User.find_by(email: email)
+    
+    # @user のパスワードが合っているか、@user.authenticate(password) で確認
+    if @user && @user.authenticate(password)
+      # ログイン成功
+      # session[:user_id] にログインユーザの id が代入された時点でログイン完了
+      # ブラウザの Cookie にもログイン情報が保存されます。
+      session[:user_id] = @user.id
+      return true
+    else
+      # ログイン失敗
+      return false
+    end
   end
 end
