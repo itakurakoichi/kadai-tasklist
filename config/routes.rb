3797f7b354320d2054adf57b2e6559f107c3003f
jiro.kirameki <itakurakoichi.s@gmail.com>
2018-01-02 15:15:22 +0000

    ログインのrouting設定、コントローラー生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # トップページのrouting設定
   root to: 'toppages#index'
+  
+  # ログイン機能のrouting設定
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   # user登録のrouting設定
   get 'signup', to: 'users#new'
