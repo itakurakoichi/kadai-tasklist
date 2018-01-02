ae15ca97bd2ea08b37be02514fbf9dfc129b68a8
jiro.kirameki <itakurakoichi.s@gmail.com>
2018-01-02 07:06:24 +0000

    user登録のrouterの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # root to: 'tasks#index'
+  # トップページのrouting設定
   root to: 'toppages#index'
   
+  # user登録のrouting設定
+  get 'signup', to: 'users#new'
+  # 任意のrouterのみ追加
+  resources :users, only: [:index, :show, :new, :create]
+  
+  # タスクのrouting設定
   resources :tasks
 end
